2178faf1b88cb02349e265ce91e3f844bb527d33
Alan James <alanjames1987@gmail.com>
2015-06-05 17:52:11 -0400

    Displays the date it is counting down to.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 (function($) {
 
+	var date = 'January 1 2016';
+
 	$('#countdown').countdown({
-		date: 'January 1 2016',
-		render: function(data) {
+		'date': date,
+		'render': function(data) {
 			var day = '<span class="day">' + this.leadingZeros(data.days, 2) + ' DAYS<br /></span>';
 			var hour = '<span class="hour">' + this.leadingZeros(data.hours, 2) + ' HOURS<br /></span>';
 			var min = '<span class="min">' + this.leadingZeros(data.min, 2) + ' MINUTES<br /></span>';
 			var sec = '<span class="sec">' + this.leadingZeros(data.sec, 2) + ' SECONDS<br /></span>';
-			$(this.el).html(day + hour + min + sec);
+			var until = '<span> UNTIL ' + date + '<br /></span>';
+			$(this.el).html(day + hour + min + sec + until);
 		},
 	});
 
